999276181cf9bd559b79d0f4d64fb3c8791410f0
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-20 00:02:57 +0200

    Update podman-run-genie.bash

--- podman-run-genie.bash
+++ podman-run-genie.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t genie:demo .
 podman run --interactive --tty -p 8000:8000 genie:demo
-echo "browse http://localhost:8000/"
+echo "browse http://localhost:8000/hello?name=test"
