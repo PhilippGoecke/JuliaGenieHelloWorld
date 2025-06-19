575be7520c41dff6e65b747b9f930de741117a77
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-19 22:49:30 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,12 +1,12 @@
 import Pkg;
 Pkg.add("Genie")
 
-using Genie
+using Genie, Genie.Renderer.Html
 
 route("/hello") do
   name = params(:name, "World")
 
-  "Hello $name!"
+  html("Hello $name!")
 end
 
 up(8000, '0.0.0.0', async = false)
