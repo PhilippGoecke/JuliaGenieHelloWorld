bc60a53e1b70522a50b57df57401c20f0764ade8
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-20 10:17:59 +0200

    Update hello.jl

--- hello.jl
+++ hello.jl
@@ -3,10 +3,16 @@ Pkg.add("Genie")
 
 using Genie
 
-route("/hello") do
+function greet()
   name = params(:name, "World")
 
   "Hello $(name)!"
+end
+
+route("/", greet) 
+
+route("/hello") do
+  greet()
 end
 
 up(8000, "0.0.0.0", async = false)
